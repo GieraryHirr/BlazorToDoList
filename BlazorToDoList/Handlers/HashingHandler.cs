6dc6bf19147213366eb9344d90ce7aa30bae94a9
Oskar Kulpa <oskarkulpa99@gmail.com>
2024-02-28 22:49:58 +0100

    Implement HashingHandler instead of Md5HashingHandler

---
+++ BlazorToDoList/Handlers/HashingHandler.cs
@@ -0,0 +1,57 @@
+﻿using BlazorToDoList.Enums;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlazorToDoList.Handlers;
+public static class HashingHandler
+{
+    public static string Md5Hashing(string value, HashFormat format)
+    {
+        var valueBytes = Encoding.ASCII.GetBytes(value);
+        var hashedValue = MD5.Create().ComputeHash(valueBytes);
+        return ConvertToHashFormat(hashedValue, format);
+    }
+
+    public static string Sha256Hashing(string value, HashFormat format)
+    {
+        var valueBytes = Encoding.ASCII.GetBytes(value);
+        var hashedValue = SHA256.Create().ComputeHash(valueBytes);
+        return ConvertToHashFormat(hashedValue, format);
+    }
+
+    public static string Hmac256Hashing(string value, HashFormat format)
+    {
+        var valueBytes = Encoding.ASCII.GetBytes(value);
+        var hmac = new HMACSHA256();
+        hmac.Key = "OskarKey"u8.ToArray();
+        var hashedValue = hmac.ComputeHash(valueBytes);
+        return ConvertToHashFormat(hashedValue, format);
+    }
+
+    public static string Pbkbf2Hashing(string value, HashFormat format, string salt, string hashingAlgorithm)
+    {
+        var valueBytes = Encoding.ASCII.GetBytes(value);
+        var saltAsBytesArray = Encoding.ASCII.GetBytes(salt);
+        var hashedAlgorithm = new HashAlgorithmName(hashingAlgorithm);
+        var hashedValue = Rfc2898DeriveBytes
+            .Pbkdf2(valueBytes, saltAsBytesArray, 10, hashedAlgorithm, 32);
+        return ConvertToHashFormat(hashedValue, format);
+    }
+
+    public static string BCryptHashing(string textToHash) =>
+         BCrypt.Net.BCrypt.HashPassword(textToHash, 10, true);
+
+    public static bool BCryptVerify(string textToVerify, string hashedText) =>
+        BCrypt.Net.BCrypt.Verify(textToVerify, hashedText, true);
+
+    private static string ConvertToHashFormat(byte[] hashedValue, HashFormat hashFormat) =>
+        hashFormat switch
+        {
+            HashFormat.Base64 => Convert.ToBase64String(hashedValue),
+            HashFormat.Utf => Encoding.UTF8.GetString(hashedValue),
+            HashFormat.Hex => BitConverter.ToString(hashedValue).Replace("-", string.Empty),
+            HashFormat.ByteString => string.Join(" ", hashedValue.Select(b => b.ToString())),
+            HashFormat.ByteArray => string.Join(", ", hashedValue),
+            _ => throw new ArgumentException("Invalid output format specified.", nameof(hashFormat))
+        };
+}
--- BlazorToDoList/Services/CprService.cs
+++ BlazorToDoList/Services/CprService.cs
@@ -1,8 +1,8 @@
-﻿using BlazorToDoList.Models;
+﻿using BlazorToDoList.Enums;
+using BlazorToDoList.Models;
 using BlazorToDoList.Repositories.Interfaces;
 using BlazorToDoList.Handlers;
 using BlazorToDoList.Services.Interfaces;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace BlazorToDoList.Services;
 
@@ -16,7 +16,7 @@ public class CprService(ICprRepository cprRepository) : ICprService
 
     public async Task<string?> Add(string username, string cprNo)
     {
-        var newCprNoHashed = Md5HashingHandler.Hash(cprNo);
+        var newCprNoHashed = HashingHandler.Md5Hashing(cprNo, HashFormat.Base64);
 
         var cpr = await cprRepository.GetCprByUsername(username);
         if (cpr != null )
